7a1536f4843925cbd3e9d07d195763637f2e72d0
Saroj Rout <sarojrout@Sarojs-MacBook-Pro.local>
2016-06-04 18:25:22 -0700

    advanced Tip Calculator

--- Innovative Calculator/FirstViewController.swift
+++ Innovative Calculator/FirstViewController.swift
@@ -8,8 +8,30 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var txtAddFirstNumber: UITextField!
+    @IBOutlet weak var txtAddsecNumber: UITextField!
+    @IBOutlet weak var txtMultFirstNumber: UITextField!
+    @IBOutlet weak var txtMulSecNumber: UITextField!
+    @IBOutlet weak var txtDivFirstNumber: UITextField!
+    @IBOutlet weak var txtDivSecNumber: UITextField!
+    @IBOutlet weak var lblAdditionAnswer: UILabel!
+    @IBOutlet weak var lblMulAnswer: UILabel!
+    @IBOutlet weak var lblDivAnswer: UILabel!
+    
+    var additionFirstNumber = ""
+    var additionSecNUmber = ""
+    var additionAnswer: Float = 0
+    
+    var multiFirstNumber = ""
+    var multiSecNumber = ""
+    var multiAnswer: Float=0
+    
+    var divFirstNumber = ""
+    var divSecNumber = ""
+    var divAnswer:Float=0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +41,64 @@ class FirstViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    @IBAction func btnAdditionCalculate(sender: UIButton) {
+        additionFirstNumber=txtAddFirstNumber.text
+        additionSecNUmber=txtAddsecNumber.text
+        
+        var fFirstNumber = (additionFirstNumber as NSString).floatValue
+        var fSecNumber = (additionSecNUmber as NSString).floatValue
+        additionAnswer = fFirstNumber + fSecNumber
+        
+        lblAdditionAnswer.text = "\(additionAnswer)"
+        txtAddFirstNumber.resignFirstResponder()
+        txtAddsecNumber.resignFirstResponder()
+        
+    }
+    @IBAction func btnAdditionClear(sender: UIButton) {
+        txtAddFirstNumber.text=""
+        txtAddsecNumber.text=""
+        lblAdditionAnswer.text="Answer"
+    }
+    
+    @IBAction func btnMultiCalculate(sender: UIButton) {
+      multiFirstNumber=txtMultFirstNumber.text
+      multiSecNumber = txtMulSecNumber.text
+      var fFirstNumber = (multiFirstNumber as NSString).floatValue
+      var fSecNumber = (multiSecNumber as NSString).floatValue
+      multiAnswer = fFirstNumber * fSecNumber
+        
+      lblMulAnswer.text = "\(multiAnswer)"
+      txtMultFirstNumber.resignFirstResponder()
+      txtMulSecNumber.resignFirstResponder()
+    }
+    
+    @IBAction func btnMultiClear(sender: UIButton) {
+        txtMultFirstNumber.text=""
+        txtMulSecNumber.text=""
+        lblMulAnswer.text="Answer"
+        
+    }
+    @IBAction func btnDivCalculate(sender: UIButton) {
+        divFirstNumber = txtDivFirstNumber.text
+        divSecNumber = txtDivSecNumber.text
+        
+        var fFirstNumber = (divFirstNumber as NSString).floatValue
+        var fSecNumber = (divSecNumber as NSString).floatValue
+        divAnswer = fFirstNumber / fSecNumber
+        
+        lblDivAnswer.text = "\(divAnswer)"
+        
+        txtDivFirstNumber.resignFirstResponder()
+        txtDivSecNumber.resignFirstResponder()
+    }
 
+    @IBAction func btnDIvClear(sender: UIButton) {
+        txtDivFirstNumber.text=""
+        txtDivSecNumber.text=""
+        lblDivAnswer.text="Answer"
+
+    }
 }
 
--- Innovative Calculator/SecondViewController.swift
+++ Innovative Calculator/SecondViewController.swift
@@ -8,8 +8,16 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var txtSubFirstNumber: UITextField!
+    @IBOutlet weak var txtSubSecNumber: UITextField!
+    @IBOutlet weak var lblSubstAnswer: UILabel!
+    
+    var subFirstNumber=""
+    var subSecNumber = ""
+    var subAnswer: Float=0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,27 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnSubCalculate(sender: UIButton) {
+       subFirstNumber = txtSubFirstNumber.text
+       subSecNumber = txtSubSecNumber.text
+        
+       var fFirstNumber = (subFirstNumber as NSString).floatValue
+       var fSecNumber = (subFirstNumber as NSString).floatValue
+       subAnswer = fFirstNumber - fSecNumber
+       
+       lblSubstAnswer.text = "\(subAnswer)"
+    }
+    
+    
+    @IBAction func btnSubClear(sender: UIButton) {
+        txtSubFirstNumber.text=""
+        txtSubSecNumber.text=""
+        lblSubstAnswer.text="Answer"
+    }
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        txtSubFirstNumber.resignFirstResponder()
+        txtSubSecNumber.resignFirstResponder()
+    }
 
 }
 
